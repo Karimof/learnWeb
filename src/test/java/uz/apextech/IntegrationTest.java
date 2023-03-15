d1f95da89dc08c03366518e1bba63758cebed4e3
Karimof <islamkarimov8580@gmail.com>
2023-03-15 11:36:35 +0500

    Initial version of learnWeb generated by generator-jhipster@7.9.3

---
+++ src/test/java/uz/apextech/IntegrationTest.java
@@ -0,0 +1,22 @@
+package uz.apextech;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import uz.apextech.LearnWebApp;
+import uz.apextech.config.AsyncSyncConfiguration;
+import uz.apextech.config.EmbeddedSQL;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { LearnWebApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
